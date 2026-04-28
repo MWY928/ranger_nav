14067c1995cefc847520575f17f1d1424dd0b068
MWY928 <143598076+MWY928@users.noreply.github.com>
2026-04-28 10:01:31 -0400

    d3

---
+++ start_detection_full.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/noetic/setup.bash
+source /home/mobile/catkin_ws/devel/setup.bash
+source /home/mobile/ranger_ws/devel/setup.bash
+
+source /home/mobile/miniconda3/etc/profile.d/conda.sh
+conda activate simple_nav
+
+roslaunch ranger_nav linear_info_odom.launch
