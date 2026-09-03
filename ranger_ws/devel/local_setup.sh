e2c18db0eda0dbfff0e99fd5a6a715a562895a1c
MWY928 <143598076+MWY928@users.noreply.github.com>
2026-09-03 04:50:14 -0400

    upodates

--- ranger_ws/devel/local_setup.sh
+++ ranger_ws/devel/local_setup.sh
@@ -3,7 +3,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/iaii/ranger_ws/devel}
+: ${_CATKIN_SETUP_DIR:=/home/mobile/wys/go2_falcon_main/ranger_nav_go2/ranger_ws/devel}
 CATKIN_SETUP_UTIL_ARGS="--extend --local"
 . "$_CATKIN_SETUP_DIR/setup.sh"
 unset CATKIN_SETUP_UTIL_ARGS
--- ranger_ws/devel/setup.fish
+++ ranger_ws/devel/setup.fish
@@ -18,7 +18,7 @@ if not type -q bass
 end
 
 if test -z $_CATKIN_SETUP_DIR
-    set _CATKIN_SETUP_DIR /home/iaii/ranger_ws/devel
+    set _CATKIN_SETUP_DIR /home/mobile/wys/go2_falcon_main/ranger_nav_go2/ranger_ws/devel
 end
 
 set _SETUP_UTIL "$_CATKIN_SETUP_DIR/_setup_util.py"
